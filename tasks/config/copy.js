7f4574be31a6b229b803b0fb8eea83be51ad075e
Adonis K <aklp08@gmail.com>
2015-10-26 16:35:52 +0200

    added favicon

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -6,6 +6,12 @@ module.exports = {
 				cwd: 'src/',
 				src: 'CNAME',
 				dest: 'dist/'
+			},
+			{
+				expand: true,
+				cwd: 'src/',
+				src: 'favicon.ico',
+				dest: 'dist/'
 			}
 		]
 	}
