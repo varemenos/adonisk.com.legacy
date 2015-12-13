11ae5407909f6610a110703a970a28d2fd2f58ac
Adonis K <aklp08@gmail.com>
2015-12-13 18:51:42 +0200

    users can now base64-encode files in data-url format

--- src/js/tools/base64.js
+++ src/js/tools/base64.js
@@ -1,3 +1,16 @@
+var selectInput = function (inputTypeEl) {
+	var el1 = document.querySelector('#edit');
+	var el2 = document.querySelector('#file');
+
+	if (inputTypeEl.value === 'text') {
+		el1.style.display = 'block';
+		el2.style.display = 'none';
+	} else {
+		el1.style.display = 'none';
+		el2.style.display = 'block';
+	}
+};
+
 document.addEventListener('DOMContentLoaded', function () {
 	var editor = ace.edit('edit');
 	var output = ace.edit('output');
@@ -43,6 +56,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	// var settings = document.querySelector('#settings');
 	var tabsSel = document.querySelector('select[name=tabs]');
+
+	var inputTypeEl = document.querySelector('select[name=input]');
+	var inputFileEl = document.querySelector('input[name=file_input]');
+
+	selectInput(inputTypeEl);
+	inputTypeEl.addEventListener('change', function () {
+		selectInput(inputTypeEl);
+	});
+
+	inputFileEl.addEventListener('change', function () {
+		var file = inputFileEl.files[0];
+		var reader = new FileReader();
+
+		reader.onloadend = function (f) {
+			var encoded = f.target.result;
+
+			var fileEl = document.querySelector('#file');
+			fileEl.style.backgroundImage = 'url(' + encoded + ')';
+			output.setValue(encoded, 1);
+		};
+
+		try {
+			reader.readAsDataURL(file);
+		} catch (e) {
+			console.log(e);
+		}
+	});
 
 	// settingsBtn.addEventListener('click', function () {
 	// 	if (settings.classList.contains('active')) {
